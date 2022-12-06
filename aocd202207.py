2ce3ad0f06901d41821cd0c1cb4e23da51d15ccb
Bernhard Wagner <gitcommit@nosuch.biz>
2022-12-06 10:53:10 +0100

    ADDS: Preparation for day 7

---
+++ aocd202207.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+import pytest
+
+from solution import Solution
+
+if __name__ == "__main__":
+    import sys
+
+
+class Aocd202207(Solution):
+
+    @classmethod
+    def doit(cls, data: str) -> int:
+        return 5
+
+    @classmethod
+    def doit2(cls, data):
+        return 5
+
+    def task1(self):
+        return self.doit(self.raw_input)
+
+    def task2(self):
+        return self.doit2(self.raw_input)
+
+
+# Tests #######################################################################
+
+test_data = """
+7
+5
+6
+10
+11
+""".split("\n")[1:-1]  # we need to split and slice, to comply with Solution class.
+
+test_list = zip(test_data, [5, 5, 5, 5, 5])
+
+
+@pytest.mark.parametrize("test_input, expected", test_list)
+def test_doit(test_input, expected):
+    assert Aocd202207.doit(test_input) == expected
+
+
+def test_doit_real1():
+    s = Aocd202207()
+    assert True
+
+
+def test_doit_real2():
+    s = Aocd202207()
+    assert True
+
+
+@pytest.mark.parametrize("test_input, expected", test_list)
+def test_doit2(test_input, expected):
+    assert Aocd202207.doit2(test_input) == expected
+
+
+# Main ########################################################################
+
+
+def main():
+    if len(sys.argv) > 1:
+        Aocd202207.test()
+    else:
+        s = Aocd202207()
+        print(f"task 1: {s.task1()}")
+        print(f"task 2: {s.task2()}")
+
+
+if __name__ == "__main__":
+    main()
